c58e023111e266102907a9d02e7b6eef16ecfc46
Joy Anis <joyanis00@gmail.com>
2025-07-18 01:36:25 +0300

    Implement Hive support for Note model and add generated adapter

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,15 @@
-class Note {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class Note extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String content;
+  @HiveField(2)
   final DateTime createdAt;
+  @HiveField(3)
   final int color;
 
   Note({
