ecc83109245eddf0fbbeb32fa72490b64d86c3c2
Joy Anis <joyanis00@gmail.com>
2025-07-18 00:58:17 +0300

    Add Note model with title, content, createdAt, and color properties

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class Note {
+  final String title;
+  final String content;
+  final DateTime createdAt;
+  final int color;
+
+  Note({
+    required this.title,
+    required this.content,
+    required this.createdAt,
+    required this.color,
+  });
+}
