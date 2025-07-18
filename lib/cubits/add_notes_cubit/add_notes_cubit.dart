68da576ab46c537e7f42f86c541c718468fb6134
Joy Anis <joyanis00@gmail.com>
2025-07-18 14:08:30 +0300

    Refactor addNote method in AddNotesCubit to accept Note object

--- lib/cubits/add_notes_cubit/add_notes_cubit.dart
+++ lib/cubits/add_notes_cubit/add_notes_cubit.dart
@@ -1,15 +1,17 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:note_app/cubits/add_notes_cubit/add_notes_states.dart';
+import 'package:note_app/models/note_model.dart';
 
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
 
-  Future<void> addNote(String title, String content) async {
+  addNote(Note note) async {
     emit(AddNotesLoading());
     try {
-    
-      await Future.delayed(const Duration(seconds: 1));
+      final notesBox = Hive.box<Note>('notes');
       emit(AddNotesSuccess());
+      await notesBox.add(note);
     } catch (e) {
       emit(AddNotesFailure(e.toString()));
     }
