8eae6d4efa5159972b5527efc0bc4e3b257ffa77
Joy Anis <joyanis00@gmail.com>
2025-07-18 13:52:17 +0300

    create add notes states

---
+++ lib/cubits/add_notes_cubit/add_notes_states.dart
@@ -0,0 +1,13 @@
+abstract class AddNotesState {}
+
+class AddNotesInitial extends AddNotesState {}
+
+class AddNotesLoading extends AddNotesState {}
+
+class AddNotesSuccess extends AddNotesState {}
+
+class AddNotesFailure extends AddNotesState {
+  final String error;
+
+  AddNotesFailure(this.error);
+}
